ef33e95fc380dde26980f82a2cced6ee5a469362
ifconfigbrian <njugunabriian@gmail.com>
2024-05-01 01:33:36 +0300

    first release of main class

---
+++ EmailAppMain.java
@@ -0,0 +1,8 @@
+public class EmailAppMain {
+
+    public static void main(String[] args) {
+        EmailApp em1 = new EmailApp("Brian", "Njuguna");
+        System.out.println(em1.showInfo()); 
+
+    }
+}
