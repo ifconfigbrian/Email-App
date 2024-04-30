367d2515325ca4ce31016344fe39eca7e83ac2a1
ifconfigbrian <njugunabriian@gmail.com>
2024-05-01 01:56:47 +0300

    removed parameters from the instance of email app

--- EmailAppMain.java
+++ EmailAppMain.java
@@ -1,7 +1,7 @@
 public class EmailAppMain {
 
     public static void main(String[] args) {
-        EmailApp em1 = new EmailApp("Brian", "Njuguna");
+        EmailApp em1 = new EmailApp();
         System.out.println(em1.showInfo()); 
 
     }
